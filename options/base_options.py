7f3711b9e24dfcc42b241be14c01bc96201b64dd
Levin Dabhi <levindabhi17@gmail.com>
2020-10-15 17:41:36 +0530

    Update base_options.py

--- options/base_options.py
+++ options/base_options.py
@@ -50,8 +50,10 @@ class BaseOptions():
 
         self.parser.add_argument('--resize_or_crop', type=str, default='scale_width',
                                  help='scaling and cropping of images at load time [resize_and_crop|crop|scale_width|scale_width_and_crop]')
+        '''
         self.parser.add_argument('--serial_batches', action='store_true',
                                  help='if true, takes images in order to make batches, otherwise takes them randomly')
+        '''
         self.parser.add_argument('--no_flip', action='store_true',
                                  help='if specified, do not flip the images for data argumentation')
         self.parser.add_argument(
